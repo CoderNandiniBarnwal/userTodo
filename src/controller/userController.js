798b7794aac9133f24cfe14680ffbb82e900d260
Nandini Barnwal <tanunandini0@gmail.com>
2026-01-05 17:30:53 +0530

    login user done

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -15,24 +15,76 @@ export const register = async (req, res) => {
       });
     }
     const hashPassword = await bcrypt.hash(password, 10);
-    const data = await userSchema.create({
+    const user = await userSchema.create({
       userName,
       email,
       password: hashPassword,
     });
-    const token = jwt.sign({ id: data._id }, process.env.secretKey, {
+    const token = jwt.sign({ id: user._id }, process.env.secretKey, {
       expiresIn: "5m",
     });
 
     verifyEmail(token, email);
-    data.token = token;
-    await data.save();
+
+    user.token = token;
+    await user.save();
 
     return res.status(201).json({
       success: true,
       message: "User registered Sucessfully",
-      data: data,
+      data: user,
     });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userSchema.findOne({ email: email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "unauthorized access",
+      });
+    }
+
+    const passwordCheck = await bcrypt.compare(password, user.password);
+    if (!passwordCheck) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credintials",
+      });
+    }
+
+    if (passwordCheck && user.isVerified === true) {
+      const accessToken = jwt.sign({ id: user._id }, process.env.secretKey, {
+        expiresIn: "10days",
+      });
+      const refreshToken = jwt.sign({ id: user._id }, process.env.secretKey, {
+        expiresIn: "30days",
+      });
+
+      user.isLogin = true;
+      await user.save();
+      return res.status(200).json({
+        success: true,
+        message: "Login successful",
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+        data: user,
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "complete vefication first then login",
+      });
+    }
   } catch (error) {
     return res.status(500).json({
       success: false,
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { register } from "../controller/userController.js";
+import { login, register } from "../controller/userController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const userRoute = express.Router();
 
 userRoute.post("/register", register);
 userRoute.get("/verify", verifyToken);
+userRoute.post("/login", login);
 
 export default userRoute;
